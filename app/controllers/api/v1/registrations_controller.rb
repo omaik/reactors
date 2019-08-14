b1bef25bea69521b58df96c499c05663979d241e
Olena Niemova <nemova.elena20@gmail.com>
2019-08-14 18:23:38 +0300

    DEVISE: Changed devise routes

--- app/controllers/api/v1/registrations_controller.rb
+++
@@ -1,6 +0,0 @@
-module Api
-  module V1
-    class RegistrationsController < Devise::RegistrationsController
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
+  scope '/api' do
+    scope '/v1' do
+      devise_for :users,
+                  path: '',
+                  path_names: {
+                    sign_in: 'login',
+                    sign_out: 'logout',
+                    registration: 'signup'
+                  },
+                  controllers: {
+                    sessions: 'api/v1/sessions'
+                  }
+    end
+  end
 
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
       get :hello, to: 'hellos#index'
-
-      devise_for :users,
-                 path: '',
-                 path_names: {
-                  sign_in: 'login',
-                  sign_out: 'logout',
-                  registration: 'signup'
-                 },
-                 controllers: {
-                  sessions: 'api/v1/sessions'
-                 }
 
       resource :users, only: [:show, :update]
     end
